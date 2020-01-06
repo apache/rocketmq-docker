1267b6107a84c839234d80d395952c1d8b233998
冯忠孝 <32647908@qq.com>
2020-01-06 14:14:22 +0800

    - 调小内存,让用户可以在第一次顺利启动测试 - 修复配置文件conf/broker.conf的path 笔误

--- image-build/scripts/runserver-customize.sh
+++ image-build/scripts/runserver-customize.sh
@@ -129,7 +129,8 @@ Xms=$MAX_HEAP_SIZE
 Xmx=$MAX_HEAP_SIZE
 Xmn=$HEAP_NEWSIZE
 # Set for `JAVA_OPT`.
-JAVA_OPT="${JAVA_OPT} -server -Xms${Xms} -Xmx${Xmx} -Xmn${Xmn}"
+#JAVA_OPT="${JAVA_OPT} -server -Xms${Xms} -Xmx${Xmx} -Xmn${Xmn}"
+JAVA_OPT="${JAVA_OPT} -server -Xms256m -Xmx1g -Xmn256m"
 JAVA_OPT="${JAVA_OPT} -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=70 -XX:+CMSParallelRemarkEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8  -XX:-UseParNewGC"
 JAVA_OPT="${JAVA_OPT} -verbose:gc -Xloggc:/dev/shm/rmq_srv_gc.log -XX:+PrintGCDetails"
 JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
